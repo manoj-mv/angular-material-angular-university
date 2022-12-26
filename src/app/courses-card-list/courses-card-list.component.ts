5722dca5e3866dea52092bc2ea2556ff38e37e51
Manoj M V <manojmv3019@gmail.com>
2022-12-26 18:14:24 +0530

    stepper,some form elements

--- src/app/courses-card-list/courses-card-list.component.ts
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -1,28 +1,27 @@
-import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
-import {Course} from "../model/course";
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { Course } from "../model/course";
 import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
 
 @Component({
-    selector: 'courses-card-list',
-    templateUrl: './courses-card-list.component.html',
-    styleUrls: ['./courses-card-list.component.css']
+  selector: 'courses-card-list',
+  templateUrl: './courses-card-list.component.html',
+  styleUrls: ['./courses-card-list.component.css']
 })
 export class CoursesCardListComponent implements OnInit {
 
-    @Input()
-    courses: Course[];
+  @Input()
+  courses: Course[];
 
-    constructor() {
-    }
+  constructor() {
+  }
 
-    ngOnInit() {
+  ngOnInit() {
 
-    }
+  }
 
-    editCourse(course:Course) {
+  editCourse(course: Course) {
 
-
-    }
+  }
 
 }
 
