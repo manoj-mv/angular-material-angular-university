ecb30b1271d31fb56e20648c4efb6eeaa8a90f37
Manoj M V <manojmv3019@gmail.com>
2023-01-03 14:22:30 +0530

    flat ree and nested tree implemented

--- src/app/tree-demo/tree-demo.component.ts
+++ src/app/tree-demo/tree-demo.component.ts
@@ -1,11 +1,18 @@
-import {Component, OnInit} from '@angular/core';
-import {MatTreeFlatDataSource, MatTreeFlattener, MatTreeNestedDataSource} from '@angular/material/tree';
-import {FlatTreeControl, NestedTreeControl} from '@angular/cdk/tree';
+import { Component, OnInit } from '@angular/core';
+import { MatTreeFlatDataSource, MatTreeFlattener, MatTreeNestedDataSource } from '@angular/material/tree';
+import { FlatTreeControl, NestedTreeControl } from '@angular/cdk/tree';
 
 interface CourseNode {
   name: string;
   children?: CourseNode[];
 }
+
+interface CourseFlatNode {
+  name: string;
+  expandable: boolean;
+  level: number;
+}
+
 
 const TREE_DATA: CourseNode[] = [
   {
@@ -57,13 +64,43 @@ const TREE_DATA: CourseNode[] = [
   styleUrls: ['tree-demo.component.scss']
 })
 export class TreeDemoComponent implements OnInit {
+  // Nested tree
+  nestedDataSource = new MatTreeNestedDataSource<CourseNode>();
+  nestedTreeControl = new NestedTreeControl<CourseNode>(node => node.children);
 
+  // Flat tree
+  flatTreeControl = new FlatTreeControl<CourseFlatNode>(
+    node => node.level,
+    node => node.expandable
+  );
+
+  treeFlatner = new MatTreeFlattener(
+    (node: CourseNode, level: number): CourseFlatNode => {
+      return {
+        name: node.name,
+        expandable: node.children?.length > 0,
+        level
+      }
+    },
+    node => node.level,
+    node => node.expandable,
+    node => node.children
+  );
+
+  flatDataSource = new MatTreeFlatDataSource(this.flatTreeControl, this.treeFlatner)
 
   ngOnInit() {
-
-
+    this.nestedDataSource.data = TREE_DATA;
+    this.flatDataSource.data = TREE_DATA;[]
   }
 
+  hasNestedChild(index: number, node: CourseNode): boolean {
+    return node?.children?.length > 0;
+  }
+
+  hasFlatChild(index: number, node: CourseFlatNode): boolean {
+    return node.expandable;
+  }
 }
 
 
